6cda6609c60f1072acc49fdf791a544f29b0818a
VadimL <badgunn@yandex.ru>
2023-07-12 00:30:34 +0400

    <fix> ТЗ 15 устранил замечания после первого ревью. Побеждаем checkstyle v1

--- src/test/java/ru/practicum/shareit/request/ItemRequestControllerTest.java
+++ src/test/java/ru/practicum/shareit/request/ItemRequestControllerTest.java
@@ -18,7 +18,6 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
